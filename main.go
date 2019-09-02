49135cc12980208c114c73618456676ee3e5ad9a
fo2rist <fo2@inbox.ru>
2019-09-02 11:26:36 -0700

    Add iml for Intellij Idea to project

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"os"
 )
 
-func getHttp(addr string) string {
+func getHTTP(addr string) string {
 	resp, err := http.Get(addr)
 	if err != nil {
 		return ""
@@ -32,32 +32,32 @@ func main() {
 	router.Use(gin.Logger())
 
 	router.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, string([]byte("<html>**Formula 1 History API!** Proxy for ergast.com <a href=\"https://twitter.com/share?ref_src=twsrc%5Etfw\" class=\"twitter-share-button\" data-show-count=\"false\">Tweet</a><script async src=\"https://platform.twitter.com/widgets.js\" charset=\"utf-8\"></script></html>")))
+		c.String(http.StatusOK, string([]byte("<html>**Formula 1 History API!** Proxy for ergast.com")))
 	})
 
 	router.GET("/drivers", func(c *gin.Context) {
 		c.String(http.StatusOK,
-			getHttp("http://ergast.com/api/f1/drivers.json?limit=900&offset=00"))
+			getHTTP("http://ergast.com/api/f1/drivers.json?limit=900&offset=00"))
 	})
 
 	router.GET("/circuits", func(c *gin.Context) {
 		c.String(http.StatusOK,
-			getHttp("http://ergast.com/api/f1/circuits.json?limit=100&offset=00"))
+			getHTTP("http://ergast.com/api/f1/circuits.json?limit=100&offset=00"))
 	})
 
 	router.GET("/teams", func(c *gin.Context) {
 		c.String(http.StatusOK,
-			getHttp("http://ergast.com/api/f1/constructors.json?limit=2500&offset=00"))
+			getHTTP("http://ergast.com/api/f1/constructors.json?limit=2500&offset=00"))
 	})
 
 	router.GET("/standing", func(c *gin.Context) {
 		c.String(http.StatusOK,
-			getHttp("http://ergast.com/api/f1/current/driverStandings.json"))
+			getHTTP("http://ergast.com/api/f1/current/driverStandings.json"))
 	})
 
 	router.GET("/season", func(c *gin.Context) {
 		c.String(http.StatusOK,
-			getHttp("http://ergast.com/api/f1/current.json"))
+			getHTTP("http://ergast.com/api/f1/current.json"))
 	})
 
 	router.Run(":" + port)
