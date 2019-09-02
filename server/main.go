92f2a1f8e4031208209f3d82c1649a49aed2542b
fo2rist <fo2@inbox.ru>
2019-09-01 22:17:59 -0700

    Add proxy endpoints for ergast API

--- server/main.go
+++ server/main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
-	"net/http"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 )
+
+func getHttp(addr string) string {
+	resp, err := http.Get(addr)
+	if err != nil {
+		return ""
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return ""
+	}
+	return string(body)
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -17,7 +32,32 @@ func main() {
 	router.Use(gin.Logger())
 
 	router.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, string([]byte("**Hello!**")))
+		c.String(http.StatusOK, string([]byte("**Formula 1 History API!**\nProxy for ergast.com")))
+	})
+
+	router.GET("/drivers", func(c *gin.Context) {
+		c.String(http.StatusOK,
+			getHttp("http://ergast.com/api/f1/drivers.json?limit=160&offset=00"))
+	})
+
+	router.GET("/circuits", func(c *gin.Context) {
+		c.String(http.StatusOK,
+			getHttp("http://ergast.com/api/f1/circuits.json?limit=160&offset=00"))
+	})
+
+	router.GET("/teams", func(c *gin.Context) {
+		c.String(http.StatusOK,
+			getHttp("http://ergast.com/api/f1/constructors.json?limit=160&offset=00"))
+	})
+
+	router.GET("/standing", func(c *gin.Context) {
+		c.String(http.StatusOK,
+			getHttp("http://ergast.com/api/f1/current/driverStandings.json"))
+	})
+
+	router.GET("/season", func(c *gin.Context) {
+		c.String(http.StatusOK,
+			getHttp("http://ergast.com/api/f1/current.json"))
 	})
 
 	router.Run(":" + port)
